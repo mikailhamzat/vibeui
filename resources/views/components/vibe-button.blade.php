bd2c97e232628b2412e48eda4030b15b3e3fd7ec
Mikail <mikadonline@gmail.com>
2025-04-19 23:22:45 +0100

    Update vibe-button.blade.php

--- resources/views/components/vibe-button.blade.php
+++ resources/views/components/vibe-button.blade.php
@@ -1,1 +1,32 @@
+@props([
+    'variant' => 'primary',
+    'size' => 'md',
+    'icon' => null,
+    'disabled' => false,
+])
+
+@php
+    $variants = [
+        'primary' => 'bg-blue-600 text-white',
+        'secondary' => 'bg-gray-600 text-white',
+        'danger' => 'bg-red-600 text-white',
+        'outline' => 'border-2 border-gray-600 text-gray-600',
+    ];
+
+    $sizes = [
+        'sm' => 'px-3 py-2 text-sm',
+        'md' => 'px-4 py-2 text-base',
+        'lg' => 'px-6 py-3 text-lg',
+    ];
+
+    $variantClass = $variants[$variant] ?? $variants['primary'];
+    $sizeClass = $sizes[$size] ?? $sizes['md'];
+@endphp
+
+<button {{ $attributes->merge(['class' => "inline-flex items-center justify-center rounded-lg $variantClass $sizeClass", 'disabled' => $disabled]) }}>
+    @if($icon)
+        <x-dynamic-component :component="$icon" class="mr-2" />
+    @endif
+    {{ $slot }}
+</button>
 
